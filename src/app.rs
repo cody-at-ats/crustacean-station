dc02e22d2781c7e142f0a4739834cedd820b633f
Iris Jennison <tjennison@atsautomation.com>
2023-08-09 10:44:54 -0400

    took out some sample code

--- src/app.rs
+++ src/app.rs
@@ -3,8 +3,9 @@ use egui::{Frame, vec2, Rect, emath, Pos2, pos2, Shape, Stroke, Ui};
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
+
 pub struct CrustaceanStationApp {
-    // Example stuff:
+    // Example stuff: I.e the app state 
     label: String,
 
     // this how you opt-out of serialization of a member
@@ -63,57 +64,6 @@ impl eframe::App for CrustaceanStationApp {
             egui::warn_if_debug_build(ui);
         });
 
-
         // DrumSequencer::show(ctx,|ui|);
-        egui::Window::new("Drum Sequencer").show(ctx, |ui|{
-            ui.horizontal_wrapped(|ui| {
-                for _ in 0..64 {
-                    ui.checkbox(&mut false, "");
-                }
-            });
-            ui.checkbox(&mut false, "checkbox");
-        });
-
-        egui::Window::new("Scope").show(ctx, |ui| {
-            let color = if ui.visuals().dark_mode {
-                egui::Color32::from_additive_luminance(196)
-            } else {
-                egui::Color32::from_black_alpha(240)
-            };
-    
-            Frame::canvas(ui.style()).show(ui, |ui| {
-                ui.ctx().request_repaint();
-                let time = ui.input(|i| i.time);
-    
-                let desired_size = ui.available_width() * vec2(1.0, 0.35);
-                let (_id, rect) = ui.allocate_space(desired_size);
-    
-                let to_screen =
-                    emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=1.0, -1.0..=1.0), rect);                    
-    
-                let mut shapes = vec![];
-    
-                for &mode in &[2, 3, 5] {
-                    let mode = mode as f64;
-                    let n = 120;
-                    let speed = 1.5;
-    
-                    let points: Vec<Pos2> = (0..=n)
-                        .map(|i| {
-                            let t = i as f64 / (n as f64);
-                            let amp = (time * speed * mode).sin() / mode;
-                            let y = amp * (t * std::f64::consts::TAU / 2.0 * mode).sin();
-                            to_screen * pos2(t as f32, y as f32)
-                        })
-                        .collect();
-    
-                    let thickness = 10.0 / mode as f32;
-                    shapes.push(Shape::line(points, Stroke::new(thickness, color)));
-                }
-    
-                ui.painter().extend(shapes);
-            });
-     
-        });
     }
 }
