6ec3001203478ef7b4d53c85a685751b43265bf2
Cody Howarth <chowarth@atsautomation.com>
2023-08-09 23:11:27 -0400

    Fix - more warnings

--- src/app/sample_import.rs
+++ src/app/sample_import.rs
@@ -1,20 +1,14 @@
-
-
-
-
 use wasm_bindgen::prelude::*;
 
+// TODO: save these for later
+// #[wasm_bindgen(module = "/src/app/javascript/play_wav_file.js")]
+// extern "C" {
+//     fn selectAndPlayWavFile();
+// }
 
-
-#[wasm_bindgen(module = "/src/app/javascript/play_wav_file.js")]
-extern "C" {
-
-    fn selectAndPlayWavFile();
-}
-
-pub fn play_wav() {
-    unsafe { selectAndPlayWavFile() };
-}
+// pub fn play_wav() {
+//     selectAndPlayWavFile();
+// }
 
 #[wasm_bindgen(module = "/src/app/javascript/play_wav_file.js")]
 extern "C" {
@@ -28,8 +22,11 @@ macro_rules! play_wav_from_assets {
             // Include the .wav file as a byte array
             let wav_file_bytes: &[u8] = include_bytes!($asset_path);
 
+            // Import the base64 crate within the macro definition
+            use base64::Engine;
+
             // Encode the byte array as a base64 string
-            let base64_string = base64::encode(wav_file_bytes);
+            let base64_string = base64::engine::general_purpose::STANDARD.encode(wav_file_bytes);
 
             // Pass the base64 string to a JavaScript function that decodes it and plays the audio
             play_wav_from_base64_string(&base64_string);
